b21ce56022ecace7e641f7416c8bd006a656b198
Jacob Hornung <hornunjb@mail.uc.edu>
2021-03-04 14:44:45 -0500

    setting up retrofit

---
+++ app/src/main/java/edu/uc/hornunjb/weatherwear/dao/IWeatherDAO.kt
@@ -0,0 +1,13 @@
+package edu.uc.hornunjb.weatherwear.dao
+import edu.uc.hornunjb.weatherwear.dto.WeatherResponse
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface IWeatherDAO {
+
+    @GET("data/2.5/weather?")
+    fun getCurrentWeatherData(@Query("lat") lat: String, @Query("lon") lon: String, @Query("APPID") app_id: String): Call<WeatherResponse>
+}
