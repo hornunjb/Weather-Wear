95b59c3df9c0e0007f5bfae4aa62432954f27b67
Jacob Hornung <hornunjb@mail.uc.edu>
2021-04-04 14:51:56 -0400

    fixed styling and created add button functionality

--- app/src/main/java/edu/uc/hornunjb/weatherwear/RecommendationsListActivity.kt
+++ app/src/main/java/edu/uc/hornunjb/weatherwear/RecommendationsListActivity.kt
@@ -1,11 +1,20 @@
 package edu.uc.hornunjb.weatherwear
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 
 class RecommendationsListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recommendations_list)
+
+        val button = findViewById<ImageButton>(R.id.imgBtnAdd)
+        button.setOnClickListener{
+            val intent = Intent(this, CreateRecommendationActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
