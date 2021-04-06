396e06c7334de4171603cfa8a0778bc4570041fe
AWalderDev <walderaj@mail.uc.edu>
2021-04-06 16:07:29 -0400

    Changed onClick listeners to use Kotlin Extension method of finding ui objects.

--- app/src/main/java/edu/uc/hornunjb/weatherwear/RecommendationsListActivity.kt
+++ app/src/main/java/edu/uc/hornunjb/weatherwear/RecommendationsListActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import kotlinx.android.synthetic.main.activity_recommendations_list.*
 
 class RecommendationsListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,8 +13,7 @@ class RecommendationsListActivity : AppCompatActivity() {
         setContentView(R.layout.activity_recommendations_list)
 
         //Clicking the '+' icon will bring you to the CreateRecommendationActivity, where a user creates a new recommendation
-        val btnAdd = findViewById<ImageButton>(R.id.imgBtnAdd)
-        btnAdd.setOnClickListener{
+        imgBtnAdd.setOnClickListener{
             val intent = Intent(this, CreateRecommendationActivity::class.java)
             startActivity(intent)
         }
